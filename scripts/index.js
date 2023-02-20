081caeeaabbbe3ab03dbd5523fe0edf9f5957dc5
Alexey Odoevsky <AAOdoevsky@yandex.ru>
2023-02-20 20:34:17 +0300

    feat: project 5, iteration 1/4

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
 const nameTitle = document.querySelector('.profile__title');
 const aboutSubtitle = document.querySelector('.profile__subtitle');
-const popupEdit = document.querySelector('.popup__edit');
+const popupEdit = document.querySelector('.popup_edit');
 const formEditElement = document.querySelector('.popup__form-edit');
 const closeEditButton = popupEdit.querySelector('.popup__close-edit');
 
@@ -11,10 +11,10 @@ const addButton = document.querySelector('.profile__add-button');
 const placeNameInput = document.querySelector('.popup__input_type_placename');
 const linkInput = document.querySelector('.popup__input_type_link');
 const formAddElement = document.querySelector('.popup__form-add');
-const popupAdd = document.querySelector('.popup__add');
+const popupAdd = document.querySelector('.popup_add');
 const closeAddButton = popupAdd.querySelector('.popup__close-add');
 
-const popupView = document.querySelector('.popup__view');
+const popupView = document.querySelector('.popup_view');
 const popupViewName = popupView.querySelector('.popup__image-header-view');
 const popupViewImage = popupView.querySelector('.popup__image-view');
 const popupViewCloseButton = popupView.querySelector('.popup__close-view');
